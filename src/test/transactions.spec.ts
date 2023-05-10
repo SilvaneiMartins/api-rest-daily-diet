1bf85996c7b3d21c4eadc208460418feb9dfeb41
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-05-10 00:14:06 -0300

    Test pode criar uma transactions vitest

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { test, beforeAll, afterAll } from 'vitest';
+
+import { app } from '../app';
+
+beforeAll(async () => {
+    await app.ready();
+})
+
+afterAll(async () => {
+    await app.close();
+});
+
+test('O usuário consegue criar uma nova transação', async () => {
+    await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'New transaction',
+            value: 5000,
+            type: 'credit',
+        })
+        .expect(201);
+});
